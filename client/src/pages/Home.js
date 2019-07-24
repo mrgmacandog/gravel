6c7d1f5ef40eb1c88b39ce1529c2edad17b6d84a
LIZETHVERA <lizeth.veraro@gmail.com>
2019-07-23 21:40:24 -0700

    Post Driver and Rider

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,10 @@
 // import React from "react";
 import React, { Component } from "react";
-
 import Jumbotron from "../components/Jumbotron";
 import Input from "../components/Input";
 import Button from "../components/Button";
 import ChoiceButton from "../components/choiceButton";
-import { PostList, PostListItem } from "../components/PostList";
+// import { PostList, PostListItem } from "../components/PostList";
 import { Container, Row, Col } from "../components/Grid";
 
 
@@ -19,31 +18,35 @@ import { Container, Row, Col } from "../components/Grid";
 // export default Home;
 
 export default class Home extends Component {
-    state = {
-        posts: [],
-        postSearch: ""
-      };
-    
-      handleInputChange = event => {
-        // Destructure the name and value properties off of event.target
-        // Update the appropriate state
-        const { name, value } = event.target;
-        this.setState({
-          [name]: value
-        });
-      };
-    
-      handleFormSubmit = event => {
-        // When the form is submitted, prevent its default behavior, get posts update the posts state
-        event.preventDefault();
-        
-        // API.getPosts(this.state.postSearch)
-        //   .then(res => this.setState({ posts: res.data }))
-        //   .catch(err => console.log(err));
-      };
-    render() {
-      return (
-        <React.Fragment>
+  state = {
+    posts: [],
+    postSearch: ""
+  };
+
+  handleInputChange = event => {
+    // Destructure the name and value properties off of event.target
+    // Update the appropriate state
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    // When the form is submitted, prevent its default behavior, get posts update the posts state
+    event.preventDefault();
+
+    // API.getPosts(this.state.postSearch)
+    //   .then(res => this.setState({ posts: res.data }))
+    //   .catch(err => console.log(err));
+  };
+
+  onClick() {
+    window.location.href = "/driver";
+  }
+  render() {
+    return (
+      <React.Fragment>
         <Jumbotron />
         <Container>
           <Row>
@@ -74,55 +77,27 @@ export default class Home extends Component {
             </Col>
           </Row>
           <Row>
-            <Col size="xs-12">
-              {!this.state.posts.length ? (
-                <h3 className="text-center">No posts to Display</h3>
-              ) : (
-                <PostList>
-                  {this.state.posts.map(post => {
-                    return (
-                      <PostListItem
-                        key={post.title}
-                        title={post.title}
-                        start_location={post.start_location}
-                        end_location={post.end_location}
-                        leaving_date={post.leaving_date}
-                        flexible_date={post.flexible_date}
-                        cost={post.cost}
-                        seats_available={post.seats_available}
-                        smoking={post.smoking}
-                        luggage={post.luggage}
-                        comments={post.comments}                        
-                      />
-                    );
-                  })}
-                </PostList>
-              )}
-            </Col>
-          </Row>
-          <Row>
-             <Col size="sm-6">
-                <ChoiceButton
-                 onClick={this.handleChangePage}
-                  type="success"
-                  className="driver-input-lg"
-                  text="Driver"
-                >
-                </ChoiceButton>
+            <Col size="sm-6">
+              <ChoiceButton
+                onClick={() => window.location.href = "/driver"}
+                type="success"
+                className="driver-input-lg"
+                text="Driver"
+              >
+              </ChoiceButton>
             </Col>
             <Col size="sm-6">
-                <ChoiceButton
-                //   onClick={this}
-                  type="success"
-                  className="rider-input-lg"
-                  text="Rider"
-                 
-                >
-                </ChoiceButton>
+              <ChoiceButton
+                onClick={() => window.location.href = "/rider"}
+                type="success"
+                className="rider-input-lg"
+                text="Rider"
+              >
+              </ChoiceButton>
             </Col>
-           </Row>
+          </Row>
         </Container>
-        </React.Fragment>
-      );
-    }
+      </React.Fragment>
+    );
   }
+}
