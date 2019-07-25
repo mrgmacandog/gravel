2f06361454e432b9cbeff58d5a94f30816a0c13d
mrgmacandog <24902881+mrgmacandog@users.noreply.github.com>
2019-07-24 21:13:06 -0700

    Display proper info for each TripItem

--- client/src/components/TripItem/index.js
+++ client/src/components/TripItem/index.js
@@ -1,19 +1,29 @@
 import React from "react";
 
 // Can change to stateful component if need be
-function TripItem() {
+function TripItem(props) {
     return (
+        // TODO: Implement for /rider. Only works for /driver currently
         <div className="card">
-            <h5 className="card-header">[Destination]</h5>
+            <h5 className="card-header">{props.trip.end_location}</h5>
             <div className="card-body">
-                <h5 className="card-title">[Leaving Date] <span className="badge badge-pill badge-danger">Not Flexible</span></h5>
-                <p className="card-text">[Cost]</p>
-                <p className="card-text">[Number of seats]</p>
+                <h5 
+                    className="card-title">{props.trip.leaving_date}
+                    {props.trip.flexible_date
+                        ? <span className="badge badge-pill badge-success">Flexible</span>
+                        : <span className="badge badge-pill badge-danger">Not Flexible</span>}
+                </h5>
+                <p className="card-text">${props.trip.cost}</p>
+                <p className="card-text">{props.trip.seats_available}</p>
 
-                <span className="badge badge-pill badge-danger">No Smoking</span>
-                <span className="badge badge-pill badge-success">Luggage Space</span>
+                {props.trip.smoking
+                    ? <span className="badge badge-pill badge-success">Smoking</span>
+                    : <span className="badge badge-pill badge-danger">No Smoking</span>}
+                {props.trip.luggage
+                    ? <span className="badge badge-pill badge-success">Luggage Space</span>
+                    : <span className="badge badge-pill badge-danger">No Luggage Space</span>}
 
-                <p className="card-text">[Comments]</p>
+                <p className="card-text">{props.trip.comment}</p>
 
                 {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
             </div>
