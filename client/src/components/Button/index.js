a8583f03657c8239ec41aec661951be46208b35c
LIZETHVERA <lizeth.veraro@gmail.com>
2019-07-25 16:06:09 -0700

    Post Driver form

--- client/src/components/Button/index.js
+++ client/src/components/Button/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 
 // Destructuring the type, className, children and onClick props, applying them to the button element
-function Button({ type = "default", className, children, onClick }) {
+function Button({ type = "default", className, children, onClick,  text}) {
   return (
     <button onClick={onClick} className={["btn btn-lg", `btn-${type}`, className].join(" ")}>
       {children}
+      {text}
     </button>
   );
 }
