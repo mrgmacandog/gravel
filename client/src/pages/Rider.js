0aad77bd43bf8ab7fca6da739f45e0fbafe4141c
mrgmacandog <24902881+mrgmacandog@users.noreply.github.com>
2019-08-03 11:20:40 -0700

    Change Driver/Rider comp. to be a class

--- client/src/pages/Rider.js
+++ client/src/pages/Rider.js
@@ -1,43 +1,45 @@
-import React from "react";
+import React, { Component } from "react";
 import TripContainer from "../components/TripContainer";
 import LocationInput from "../components/LocationInput";
 
 // Can change to stateful component if need be
-// Can access state with props.state
-// Can handle input change with props.handleInputChange
-function Rider(props) {
-    return (
-        <React.Fragment>
-            <div className="container">
-                <form>
-                    <div className="row">
-                        <div className="col-md-6">
-                            <LocationInput
-                                name="startLocation"
-                                value={props.state.startLocation}
-                                onChange={props.handleInputChange}
-                                placeholder="Enter the departure city or use current location"
-                                useCurrentLocation={props.useCurrentLocation}
-                            />
+// Can access state with this.props.state
+// Can handle input change with this.props.handleInputChange
+class Rider extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <div className="container">
+                    <form>
+                        <div className="row">
+                            <div className="col-md-6">
+                                <LocationInput
+                                    name="startLocation"
+                                    value={this.props.state.startLocation}
+                                    onChange={this.props.handleInputChange}
+                                    placeholder="Enter the departure city or use current location"
+                                    useCurrentLocation={this.props.useCurrentLocation}
+                                />
+                            </div>
+                            <div className="col-md-6">
+                                <LocationInput
+                                    name="endLocation"
+                                    value={this.props.state.endLocation}
+                                    onChange={this.props.handleInputChange}
+                                    placeholder="Enter the destination (optional)"
+                                    useCurrentLocation={this.props.useCurrentLocation}
+                                />
+                            </div>
                         </div>
-                        <div className="col-md-6">
-                            <LocationInput
-                                name="endLocation"
-                                value={props.state.endLocation}
-                                onChange={props.handleInputChange}
-                                placeholder="Enter the destination (optional)"
-                                useCurrentLocation={props.useCurrentLocation}
-                            />
-                        </div>
-                    </div>
 
-                    <button className="btn btn-primary" type="submit" name="action" onClick={props.getDrivers}>Search</button>
-                </form>
-            </div>
-            {/* TODO: Send TripContainer the data from Drivers table */}
-            <TripContainer page={"Rider"} loggedIn={props.state.loggedIn} results={props.state.results} showModal={props.showModal}/>
-        </React.Fragment>
-    );
+                        <button className="btn btn-primary" type="submit" name="action" onClick={this.props.getDrivers}>Search</button>
+                    </form>
+                </div>
+                {/* TODO: Send TripContainer the data from Drivers table */}
+                <TripContainer page={"Rider"} loggedIn={this.props.state.loggedIn} results={this.props.state.results} showModal={this.props.showModal}/>
+            </React.Fragment>
+        );
+    }
 }
 
 export default Rider;
