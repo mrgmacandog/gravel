f45382991edb75f225844a3adfb6b580a692d649
Casey <caseyclinga@gmail.com>
2019-08-05 10:49:42 -0700

    Stashing

--- routes/driverAPI.js
+++ routes/driverAPI.js
@@ -1,42 +1,42 @@
 const db = require("../models");
 
 module.exports = app => {
-    // If you are a driver
-    // Get all the trip
-app.get("/api/riders/", function (req, res) {
+  // Get all the trip
+  app.get("/api/drivers/", function (req, res) {
     db.Driver.find({})
+      // .populate("driver_id")
       .then(function (dbDriver) {
-  
+
         res.json(dbDriver);
-  
+
       })
       .catch(function (err) {
         res.json(err);
       })
   });
-  
+
   // Getting all the trip posted by rider, filter by start_location
-  app.get("/api/riders/:start_location", function (req, res) {
+  app.get("/api/drivers/:start_location", function (req, res) {
     db.Driver.find({ start_location: req.params.start_location })
       .then(function (dbDriver) {
-  
+
         res.json(dbDriver);
-  
+
       })
       .catch(function (err) {
         res.json(err);
       })
   });
-  
-  app.get("/api/riders/:start_location/:end_location", function (req, res) {
+
+  app.get("/api/drivers/:start_location/:end_location", function (req, res) {
     db.Driver.find({ start_location: req.params.start_location, end_location: req.params.end_location })
       .then(function (dbDriver) {
         res.json(dbDriver);
       })
   });
-  
+
   // Adding a trip
-  app.post("/api/riders", function (req, res) {
+  app.post("/api/drivers", function (req, res) {
     console.log("req.body: ", req.body);
     //req.body should be the information Driver entered when posting
     db.Driver.create(req.body)
@@ -49,64 +49,46 @@ app.get("/api/riders/", function (req, res) {
   });
 
   // Updating an existing trip
-app.post("/api/riders/:_id", function (req, res) {
-    db.Driver.updateOne(
-      { _id: req.params._id },
-  
-      // The field you want to update about
-      {
-        start_location: req.body.start_location,
-        end_location: req.body.end_location,
-        leaving_date: req.body.leaving_date,
-        cost: req.body.cost,
-        seats_available: req.body.seats_available,
-        smoking: req.body.smoking,
-        luggage: req.body.luggage,
-        comment: req.body.comment
-      }
-      .catch(function (err) {
-        console.log("error: ", err);
-      })
-    )
-    .then(function(dbDriver){
-      res.json(dbDriver)
-    })
-  })
-  
+  // app.post("/api/riders/:_id", function (req, res) {
+  //     db.Driver.updateOne(
+  //       { _id: req.params._id },
+
+  //       // The field you want to update about
+  //       {
+  //         start_location: req.body.start_location,
+  //         end_location: req.body.end_location,
+  //         leaving_date: req.body.leaving_date,
+  //         cost: req.body.cost,
+  //         seats_available: req.body.seats_available,
+  //         smoking: req.body.smoking,
+  //         luggage: req.body.luggage,
+  //         comment: req.body.comment
+  //       }
+  //       .catch(function (err) {
+  //         console.log("error: ", err);
+  //       })
+  //     )
+  //     .then(function(dbDriver){
+  //       res.json(dbDriver)
+  //     })
+  //   })
+
   // updating the rider into a trip
-  app.post("/api/riders/:_id", function(req,res){
-  
-    let userID;
-    db.User.find({loggedIn: true})
-    .then(function(user){
-      return userID = user._id
-    })
-    .catch(function (err) {
-      console.log("error: ", err);
-    });
-  
-    console.log("Current User: ", userID);
-  
-    db.Driver.updateOne(
-      {_id: req.params._id},
-      {
-        // the id will need to be user id
-        rider_id: userID
-      }
-    )
-    .then(function(dbDriver){
-      res.json(dbDriver)
-    })
-    .catch(function (err) {
-      console.log("error: ", err);
-    })
+  app.post("/api/drivers/:_id", function (req, res) {
+    db.Driver.updateOne({ _id: req.params._id }, { rider_id: req.body.rider_id })
+      .then(function (dbUser) {
+        res.json(dbUser)
+      })
+      .catch(function (err) {
+        res.json(err)
+      })
   })
-  
+
   // Deleting an existing trip
-  app.delete("/api/riders/:_id", function (req, res) {
+  app.delete("/api/drivers/:_id", function (req, res) {
     db.Driver.remove({ _id: req.params.id })
-    .then(function(dbDriver){
-      res.json(dbDriver)
-    })
+      .then(function (dbDriver) {
+        res.json(dbDriver)
+      })
   })
 }
--- routes/riderAPI.js
+++ routes/riderAPI.js
@@ -1,9 +1,9 @@
 const db = require("../models");
 
 module.exports = app => {
-    // If you are a rider
+
     // Get all the trip
-app.get("/api/drivers/", function (req, res) {
+app.get("/api/riders/", function (req, res) {
     db.Rider.find({})
       .then(function (dbRider) {
   
@@ -16,7 +16,7 @@ app.get("/api/drivers/", function (req, res) {
   });
   
   // Getting all the trip posted by rider, filter by start_location
-  app.get("/api/drivers/:start_location", function (req, res) {
+  app.get("/api/riders/:start_location", function (req, res) {
     db.Rider.find({ start_location: req.params.start_location })
       .then(function (dbRider) {
   
@@ -28,7 +28,7 @@ app.get("/api/drivers/", function (req, res) {
       })
   });
   
-  app.get("/api/drivers/:start_location/:end_location", function (req, res) {
+  app.get("/api/riders/:start_location/:end_location", function (req, res) {
     db.Rider.find({ start_location: req.params.start_location, end_location: req.params.end_location })
       .then(function (dbRider) {
         res.json(dbRider);
@@ -36,7 +36,7 @@ app.get("/api/drivers/", function (req, res) {
   });
   
   // Adding a trip
-  app.post("/api/drivers", function (req, res) {
+  app.post("/api/riders", function (req, res) {
     console.log("req.body: ", req.body);
     //req.body should be the information Driver entered when posting
     db.Rider.create(req.body)
@@ -49,61 +49,43 @@ app.get("/api/drivers/", function (req, res) {
   });
 
   // Updating an existing trip
-app.post("/api/drivers/:_id", function (req, res) {
-    db.Rider.updateOne(
-      { _id: req.params._id },
+// app.post("/api/drivers/:_id", function (req, res) {
+//     db.Rider.updateOne(
+//       { _id: req.params._id },
   
-      // The field you want to update about
-      {
-        start_location: req.body.start_location,
-        end_location: req.body.end_location,
-        leaving_date: req.body.leaving_date,
-        cost: req.body.cost,
-        seats_available: req.body.seats_available,
-        smoking: req.body.smoking,
-        luggage: req.body.luggage,
-        comment: req.body.comment
-      }
-      .catch(function (err) {
-        console.log("error: ", err);
-      })
-    )
-    .then(function(dbRider){
-      res.json(dbRider)
-    })
-  })
+//       // The field you want to update about
+//       {
+//         start_location: req.body.start_location,
+//         end_location: req.body.end_location,
+//         leaving_date: req.body.leaving_date,
+//         cost: req.body.cost,
+//         seats_available: req.body.seats_available,
+//         smoking: req.body.smoking,
+//         luggage: req.body.luggage,
+//         comment: req.body.comment
+//       }
+//       .catch(function (err) {
+//         console.log("error: ", err);
+//       })
+//     )
+//     .then(function(dbRider){
+//       res.json(dbRider)
+//     })
+//   })
   
   // updating the driver into a trip
-  app.post("/api/drivers/:_id", function(req,res){
-  
-    let userID;
-    db.User.find({loggedIn: true})
-    .then(function(user){
-      return userID = user._id
-    })
-    .catch(function (err) {
-      console.log("error: ", err);
-    });
-  
-    console.log("Current User: ", userID);
-  
-    db.Rider.updateOne(
-      {_id: req.params._id},
-      {
-        // the id will need to be user id
-        driver_id: userID
-      }
-    )
-    .then(function(dbRider){
-      res.json(dbRider)
-    })
-    .catch(function (err) {
-      console.log("error: ", err);
-    })
+  app.post("/api/riders/:_id", function (req, res) {
+    db.Rider.updateOne({ _id: req.params._id }, {driver_id: req.body.driver_id })
+      .then(function (dbUser) {
+        res.json(dbUser)
+      })
+      .catch(function (err) {
+        res.json(err)
+      })
   })
   
   // Deleting an existing trip
-  app.delete("/api/drivers/:_id", function (req, res) {
+  app.delete("/api/riders/:_id", function (req, res) {
     db.Rider.remove({ _id: req.params.id })
     .then(function(dbRider){
       res.json(dbRider)
