9125a168f00860d1aceff091abd7cb721070ac6b
KuanToto <stephencheng91@gmail.com>
2019-08-06 21:02:24 -0700

    navbar final

--- models/user.js
+++ models/user.js
@@ -11,7 +11,7 @@ const userSchema = new Schema({
 		username: { type: String, unique: true, required: true },
 		password: { type: String, unique: false, required: true },
 		loggedIn: { type: Boolean, unique: false, required: false }
-	},
+	}
 })
 
 // Define schema methods
