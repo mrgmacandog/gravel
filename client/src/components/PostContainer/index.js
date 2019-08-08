39fc2a2e49f6027aecdfc4e5beed5bcf38448b5a
mrgmacandog <24902881+mrgmacandog@users.noreply.github.com>
2019-08-07 18:32:12 -0700

    Dashboard styling

--- client/src/components/PostContainer/index.js
+++ client/src/components/PostContainer/index.js
@@ -1,21 +1,33 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import PostItem from "../PostItem";
+import "./style.css";
 
 // Can change to stateful component if need be
 function PostContainer(props) {
     // console.log(props);
 
     return (
-        <div className="container">
-            <p>{props.db === "drivers" ? "Trips you posted as a driver" : "Trips you posted as a rider"}</p>
-            <div className="row">
-                {props.page === "Dashboard"}
-                {/* Display each trip from results */}
-                {props.results.length > 0
-                    ? props.results.map(trip => <PostItem {...props} key={trip._id} trip={trip} db={props.db} />)
-                    : <p className="col-md-12">No trips yet!</p>}
+        <React.Fragment>
+            <h1 class="section-header">{props.db === "drivers" ? "Driver Posts" : "Rider Posts"}</h1>
+            <div className="container">
+                <div className="row">
+                    {props.page === "Dashboard"}
+                    {/* Display each trip from results */}
+                    {props.results.length > 0
+                        ? props.results.map(trip => <PostItem {...props} key={trip._id} trip={trip} db={props.db} />)
+                        : (
+                            <p className="col-md-12 no-trips-message">
+                                No trips yet, &nbsp;
+                                {props.db === "drivers"
+                                    ? <Link to="/driver-post">post one as a driver</Link>
+                                    : <Link to="/rider-post">post one as a rider</Link>}
+                                !
+                            </p>
+                        )}
+                </div>
             </div>
-        </div>
+        </React.Fragment>
     );
 }
 
