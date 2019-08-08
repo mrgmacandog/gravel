5528a30eae37160facea95b1dd5dc24388333507
mrgmacandog <24902881+mrgmacandog@users.noreply.github.com>
2019-08-07 19:09:30 -0700

    Create an empty card used for adding a trip

--- client/src/components/PostContainer/index.js
+++ client/src/components/PostContainer/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import PostItem from "../PostItem";
 import "./style.css";
+import NewTripCard from "../NewTripCard";
 
 // Can change to stateful component if need be
 function PostContainer(props) {
@@ -15,15 +16,20 @@ function PostContainer(props) {
                     {props.page === "Dashboard"}
                     {/* Display each trip from results */}
                     {props.results.length > 0
-                        ? props.results.map(trip => <PostItem {...props} key={trip._id} trip={trip} db={props.db} />)
-                        : (
-                            <p className="col-md-12 no-trips-message">
-                                No trips yet, &nbsp;
-                                {props.db === "drivers"
-                                    ? <Link to="/driver-post">post one as a driver</Link>
-                                    : <Link to="/rider-post">post one as a rider</Link>}
-                                !
-                            </p>
+                        ? (
+                            <React.Fragment>
+                                {props.results.map(trip => <PostItem {...props} key={trip._id} trip={trip} db={props.db} />)}
+                                <NewTripCard to={props.db === "drivers" ? "/driver-post" : "/rider-post"}/>
+                            </React.Fragment>
+                        ) : (
+                            <NewTripCard to={props.db === "drivers" ? "/driver-post" : "/rider-post"}/>
+                            // <p className="col-md-12 no-trips-message">
+                            //     No trips yet, &nbsp;
+                            //     {props.db === "drivers"
+                            //         ? <Link to="/driver-post">post one as a driver</Link>
+                            //         : <Link to="/rider-post">post one as a rider</Link>}
+                            //     !
+                            // </p>
                         )}
                 </div>
             </div>
